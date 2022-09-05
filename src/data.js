8cc30755030b9678159f5f8646d95ff546decf95
America hernandez <hsocamerica@gmail.com>
2022-09-05 10:43:15 -0500

    agrego ordenado alfabetico

--- src/data.js
+++ src/data.js
@@ -1,19 +1,57 @@
-// estas funciones son de ejemplo
 //import harryPotter from "./data/harrypotter/data.js"
 
 const filterAll = {
-houses : function (characters, house ) {
-  const filterCharacters = characters.filter((character)=>{
-    return character.house === house
-  });
-   return filterCharacters;
-},
-spells: function (spells, spellType ){
-  const filterSpells= spells.filter((spells)=>{
-      return spells.spell_type===spellType
-  })
-  return filterSpells;
-}
+  houses: function (characters, house) {
+    const filterCharacters = characters.filter((character) => {
+      return character.house === house
+    });
+    return filterCharacters;
+  },
+  spells: function (spells, spellType) {
+    const filterSpells = spells.filter((spells) => {
+      return spells.spell_type === spellType
+    })
+    return filterSpells;
+  }
 };
-console.log(filterAll);
-export default filterAll;
+//console.log(filterAll);
+
+
+function sortArray(order, hparray) {
+  // console.log(hparray)
+  if (order === "ascendente") {
+    hparray.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1
+      }
+
+      if (a.name > b.name) {
+        return 1
+      }
+
+      return 0
+
+    })
+
+  }
+  else { hparray.sort((a, b) => {
+    if (a.name < b.name) {
+      return 1
+    }
+
+    if (a.name > b.name) {
+      return -1
+    }
+
+    return 0
+
+  })
+
+  }
+  return (hparray);
+}
+
+
+
+export { filterAll, sortArray };
+
