b611e956886a1f826ad0deaf61288b37cf6a89f3
America hernandez <hsocamerica@gmail.com>
2022-09-06 12:02:38 -0500

    agrego primera parte del test

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,5 +1,5 @@
 import dataPotter from "../src/data/harrypotter/harry.json";
-import {sortArray} from "../src/data.js";
+import {sortArray,filterAll} from "../src/data.js";
 describe('dataPotter', () => {
   it('Should be an object', () => {
     expect(typeof dataPotter).toBe('object');
@@ -10,7 +10,7 @@ describe('dataPotter', () => {
   });
 
   it('probar si es characters es un arreglo', () => {
-    expect( Array.isArray (dataPotter.characters)).toBe(true);
+    expect(typeof dataPotter).toBe('object');
   });
 });
 
@@ -23,4 +23,40 @@ describe('sortArray', () => {
   it("returns arreglo vacio cuando se le llama con arreglo vacio", () => {
     expect(sortArray("ascendente", [])).toEqual([]);
   });
+  it("nos regresa el array de manera ascendente", () => {
+    const arrayDatos = [{name:"uled"},{name:"ol"},{name:"ol"},{name:"alydis"}]
+    const arrayResultado= [{name:"alydis"},{name:"ol"},{name:"ol"},{name:"uled"}]
+    expect(sortArray("ascendente",arrayDatos)).toEqual(arrayResultado);
+  });
+  it("nos regresa el array de manera descendente", () => {
+    const arrayDatos = [{name:"alydis"},{name:"ol"},{name:"ol"},{name:"uled"}]
+    const arrayResultado= [{name:"uled"},{name:"ol"},{name:"ol"},{name:"alydis"}]
+    expect(sortArray("descendente",arrayDatos)).toEqual(arrayResultado);
 });
+});
+describe('filterAll', () => {
+  it('is a function', () => {
+    expect(typeof filterAll.houses).toBe('function');
+  });
+  it('is a function', () => {
+    expect(typeof filterAll.spells).toBe('function');
+  });
+  it('debe retornar filtrado de chacarters gryffindor', () => {
+    const arrayCasas= [{house:"Hufflepuff"},{house:"Gryffindor"},{house:"Slytherin"},{house:"Ravenclaw"}]
+    const resultadosCasas = [{house:"Gryffindor"}]
+    expect( filterAll.houses(arrayCasas,"Gryffindor")).toEqual(resultadosCasas);
+  });
+  it('debe retornar filtrado de chacarters Slytherin', () => {
+    const arrayCasas= [{house:"Hufflepuff"},{house:"Hufflepuff"},{house:"Gryffindor"},{house:"Slytherin"},{house:"Ravenclaw"}]
+    const resultadosCasas = [{house:"Slytherin"}]
+    expect( filterAll.houses(arrayCasas,"Slytherin")).toEqual(resultadosCasas);
+  });
+  it('debe retornar filtrado de Spells Hex', () => {
+    const arrayCasas= [{house:"Hufflepuff"},{house:"Hufflepuff"},{house:"Gryffindor"},{house:"Slytherin"},{house:"Ravenclaw"}]
+    const resultadosCasas = [{house:"Slytherin"}]
+    expect( filterAll.houses(arrayCasas,"Slytherin")).toEqual(resultadosCasas);
+  });
+});
+
+
+
